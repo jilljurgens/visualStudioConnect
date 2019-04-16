0470e0f97b7646b706ec1d09bf3b6fb9135a8f69
Jill Jurgens <jjurgens@local.imodules.com>
2019-04-16 09:49:25 -0500

    Add project files.

---
+++ codingPractice/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace codingPractice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("are you connected?");
+        }
+    }
+}
